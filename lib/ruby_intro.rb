7a98e9af9aabba102d7682568ad47232cf93ae82
Anushka Garg <anushkagarg@tamu.edu>
2022-09-11 15:33:58 -0500

    Ruby Assignment 1

--- lib/ruby_intro.rb
+++ lib/ruby_intro.rb
@@ -3,34 +3,71 @@
 # Part 1
 
 def sum(arr)
-  # YOUR CODE HERE
+  sum_val = 0
+  #Running a for loop for adding each element
+  for val in arr
+    sum_val = sum_val + val
+  end
+  return sum_val
 end
 
 def max_2_sum(arr)
-  # YOUR CODE HERE
+  #Check if array is empty
+  if arr.empty?
+    return 0
+  #Check if array has only 1 element
+  elseif arr.length == 1
+    return arr[0]
+  else
+    return arr.max(2).sum
+  end
 end
 
 def sum_to_n?(arr, number)
-  # YOUR CODE HERE
+  for i in 0...arr.size()-1
+    for j in i+1...arr.size()
+      if arr[i] + arr[j] == number
+        return true
+      end
+    end
+  end
+  return false
 end
 
 # Part 2
 
 def hello(name)
-  # YOUR CODE HERE
+  return String.new('Hello, ').concat(name)
 end
 
 def starts_with_consonant?(string)
-  # YOUR CODE HERE
+  return /^[^aeiou\W]/i.match(string) != nil
 end
 
 def binary_multiple_of_4?(string)
-  # YOUR CODE HERE
+  if !string.empty?
+   return string.count('01') == string.size && string.to_i % 4 == 0
+  else
+    return false
+  end
 end
 
 # Part 3
 
 # Object representing a book
 class BookInStock
-  # YOUR CODE HERE
+  attr_accessor :isbn
+  attr_accessor :price
+  def initialize(isbn, price)
+    if isbn.empty? || price <= 1
+      raise ArgumentError.new("Invalid arguments passed")
+    else
+      @isbn = isbn
+      @price = price
+    end
+  end
+  def price_as_string()
+    String.new('$').concat('%.2f' % @price.to_s)
+  end
 end
+
